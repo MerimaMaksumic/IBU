232463bdeda7b21b97c977630b25c3199ab824a8
MerimaMaksumic <116004894+MerimaMaksumic@users.noreply.github.com>
2022-12-19 12:03:30 +0100

    Add files via upload

---
+++ MilitaryService.java
@@ -0,0 +1,19 @@
+public class MilitaryService implements NationalService{
+	int daysLeft; 
+	
+	public MilitaryService(int daysLeft) {
+		this.daysLeft = daysLeft; 
+	}
+
+	@Override
+	public int getDaysLeft() {
+		return this.daysLeft;
+	}
+
+	@Override
+	public void work() {
+		this.daysLeft--;
+		
+	}
+
+}
